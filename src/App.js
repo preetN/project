fce248202f316db4acb7edad087c265d997a673d
Antarpreet <pnarain82@gmail.com>
2023-08-22 21:28:07 +1000

    some changes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ function App() {
   const [isExp, setExp] = useState(false);
   const rows = [1, 2, 3];
   const cols = [1, 2, 3];
-
+  if (isExp === true) {
+    console.log("game over");
+  }
   return (
     <>
-      <div className="vh-100 d-flex align-items-center justify-content-center ">
-        <div className="bg-danger p-2 rounded">
-          {rows.map((item) => (
-            <div className="row">
-              <div className="col">
-                {cols.map((i) => (
-                  <Button isExp={isExp} setExp={setExp} />
-                ))}
+      {!isExp ? (
+        <div className="vh-100 d-flex align-items-center justify-content-center ">
+          <div className="bg-danger p-2 rounded">
+            {rows.map((item) => (
+              <div className="row">
+                <div className="col">
+                  {cols.map((i) => (
+                    <Button isExp={isExp} setExp={setExp} />
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <App />
+      )}
     </>
   );
 }
