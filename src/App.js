1c4f7620518cdb1c5e7156d1e62dbb98e565c734
Antarpreet <pnarain82@gmail.com>
2023-08-22 21:06:30 +1000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
+import { useState } from "react";
 import Button from "./components/Button";
 
 function App() {
+  const [isExp, setExp] = useState(false);
+  const rows = [1, 2, 3];
+  const cols = [1, 2, 3];
+
   return (
     <>
       <div className="vh-100 d-flex align-items-center justify-content-center ">
         <div className="bg-danger p-2 rounded">
-          <div className="row">
-            <div className="col">
-              <Button />
-              <Button />
-              <Button />
+          {rows.map((item) => (
+            <div className="row">
+              <div className="col">
+                {cols.map((i) => (
+                  <Button isExp={isExp} setExp={setExp} />
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <Button />
-              <Button />
-              <Button />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <Button />
-              <Button />
-              <Button />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,24 @@
 import React, { useState } from "react";
 import audioFile from "../assests/sound.mp3";
 import "bootstrap/dist/css/bootstrap.min.css";
-function Button() {
-  const [num, setNum] = useState("0");
+function Button({ isExp, setExp }) {
+  const [num, setNum] = useState("🎁");
+
+  const arr = ["❤️", "🌸", "🦋", "🎭", "❤️", "🚁", "🐶", "👀", "🙈", "❤️"];
   const ranNum = () => {
     const a = Math.floor(Math.random() * 10);
     const b = explode();
     if (b !== 0) {
       const audio = new Audio(audioFile);
       audio.play();
-      setNum("^-^");
+      setNum("💣");
+      setExp(true);
       return;
     }
-    setNum(a);
+    setNum(arr[a]);
   };
   const explode = () => {
     const exp = Math.round(Math.random() * 10);
-    console.log(exp);
     return exp <= 2 ? exp : 0;
   };
   return (
